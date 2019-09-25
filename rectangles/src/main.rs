f7abdb219fa267e494ff6759e26c1d1eb29d8723
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2019-09-24 20:17:50 -0400

    rectangle methods start

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,12 +4,22 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+}
+
 fn main() {
     let rect = Rectangle {width: 30, height: 50};
-    println!("The area of the rectangle is {} square pixels.", area(&rect));
+    println!("The area of the rectangle is {} square pixels.", rect.area());
     
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+
---
+++ rectangles/target/debug/deps/rectangles-4784e1c75a9414d0.d
@@ -0,0 +1,5 @@
+/home/gunk/Documents/therustbook/rectangles/target/debug/deps/rectangles-4784e1c75a9414d0.rmeta: src/main.rs
+
+/home/gunk/Documents/therustbook/rectangles/target/debug/deps/rectangles-4784e1c75a9414d0.d: src/main.rs
+
+src/main.rs:
