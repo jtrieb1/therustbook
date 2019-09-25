7bf44b89edeba5b91e7e6ce4b89bcdbaa189cf4e
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2019-09-24 20:46:55 -0400

    Understanding modules and libraries in Rust

---
+++ restaurant/target/debug/deps/restaurant-5ac89fe7628c5f61.d
@@ -0,0 +1,7 @@
+/home/gunk/Documents/therustbook/restaurant/target/debug/deps/restaurant-5ac89fe7628c5f61.rmeta: src/lib.rs
+
+/home/gunk/Documents/therustbook/restaurant/target/debug/deps/librestaurant-5ac89fe7628c5f61.rlib: src/lib.rs
+
+/home/gunk/Documents/therustbook/restaurant/target/debug/deps/restaurant-5ac89fe7628c5f61.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/gunk/Documents/therustbook/restaurant/target/debug/librestaurant.rlib: /home/gunk/Documents/therustbook/restaurant/src/lib.rs
