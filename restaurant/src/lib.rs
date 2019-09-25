d567346436de86ddb7a236ced365659a2e7393a3
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2019-09-24 21:06:21 -0400

    Further module practice, added methods for 'ordering breakfast'

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -5,21 +5,45 @@ mod front_of_house {
         fn seat_at_table() {}
     }
     
-    mod serving {
+    pub mod serving {
         fn take_order() {}
         
-        fn serve_order() {}
+        pub fn serve_order() {}
         
         fn take_payment() {}
     }
 
 }
 
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+mod back_of_house {
+    fn cook_order() {}
     
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+    
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Order breakfast in the summer with rye toast
+    let mut meal = back_of_house::Breakfast::summer("rye");
+    // Change our mind about the kind of toast we want
+    meal.toast = String::from("wheat");
+    println!("I'd like {} toast, please.", meal.toast);
+    
 
 }
